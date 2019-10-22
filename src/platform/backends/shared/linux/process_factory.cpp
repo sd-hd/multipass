3e8bea9942ea34dfd44b1605325845a6f5a57629
Chris Townsend <christopher.townsend@canonical.com>
2019-10-22 08:53:27 -0400

    process_factory: Remove libvirt backend constraint for apparmor process confinement

--- src/platform/backends/shared/linux/process_factory.cpp
+++ src/platform/backends/shared/linux/process_factory.cpp
@@ -67,11 +67,6 @@ mp::optional<mp::AppArmor> create_apparmor()
         mpl::log(mpl::Level::warning, "apparmor", "AppArmor disabled by environment variable");
         return mp::nullopt;
     }
-    else if (mp::utils::get_driver_str() == "libvirt")
-    {
-        mpl::log(mpl::Level::info, "apparmor", "libvirt backend disables Multipass' AppArmor support");
-        return mp::nullopt;
-    }
     else
     {
         mpl::log(mpl::Level::info, "apparmor", "Using AppArmor support");
